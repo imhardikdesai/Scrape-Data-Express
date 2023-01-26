9ff2acece5fba66aa652cee4a77e580e860bd4ad
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-01-26 10:44:17 +0530

    created working api

---
+++ functions/api.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const request = require('request')
+const cheerio = require('cheerio')
+const serverless = require('serverless-http')
+const app = express()
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    res.send({
+        "Greetings": "Welcome"
+    })
+})
+
+router.get('/apmc', (req, res) => {
+    request('http://www.apmcrajkot.com/daily_rates', (error, response, html) => {
+        if (!error && response.statusCode === 200) {
+            const $ = cheerio.load(html)
+            let strongTagValues = []
+            $('strong').each((i, element) => {
+                strongTagValues.push($(element).text())
+            })
+            res.send(strongTagValues)
+        }
+    })
+})
+
+// app.listen(3000, () => {
+//     console.log('Scraper running on port 3000')
+// })
+app.use('/', router)
+
+module.exports.handler = serverless(app)
