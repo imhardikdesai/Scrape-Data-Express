86f63491e74e895901fe68a0c2ae0772a07896ad
Desai Hardik <87645745+imhardikdesai@users.noreply.github.com>
2023-01-26 11:18:10 +0530

    add header files

--- functions/api.js
+++ functions/api.js
@@ -24,11 +24,6 @@ router.get('/apmc', (req, res) => {
     })
 })
 
-app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000/");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
 
 app.use('/', router)
 
